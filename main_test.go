fc6f924115d3dbc96e6845e051acd5bb6cc312bc
Kaatinga <painter@3lines.club>
2020-11-12 14:47:06 +0300

    test for Check() method is implemeted

--- main_test.go
+++ main_test.go
@@ -6,7 +6,8 @@ import (
 )
 
 const (
-	goodCard = "4561261212345464"
+	notSoGoodCard = "4561261212345464"
+	badCard1      = "1"
 )
 
 func TestNewCard(t *testing.T) {
@@ -17,15 +18,16 @@ func TestNewCard(t *testing.T) {
 		bankCard *BankCard
 		wantErr  bool
 	}{
-		{"ok",
-			goodCard,
+		{notSoGoodCard,
+			notSoGoodCard,
 			&BankCard{
-				Number:  goodCard,
+				Number:  notSoGoodCard,
 				Valid:   false,
 				Checked: false,
 			},
 			false,
 		},
+		{badCard1, badCard1, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -36,6 +38,31 @@ func TestNewCard(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.bankCard) {
 				t.Errorf("NewCard() got = %v, want %v", got, tt.bankCard)
+			}
+		})
+	}
+}
+
+func TestBankCard_Check(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		card  BankCard
+		want    bool
+		wantErr bool
+	}{
+		{"ok", BankCard{Number: notSoGoodCard}, false, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.card.Check()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Check() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Check() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
