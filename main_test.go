1b15ec26ab400bef4273b52f2b605b52b4667c29
Kaatinga <painter@3lines.club>
2020-11-12 12:36:14 +0300

    test for NewCard() added

--- main_test.go
+++ main_test.go
@@ -1,1 +1,42 @@
 package luhncheck
+
+import (
+	"reflect"
+	"testing"
+)
+
+const (
+	goodCard = "4561261212345464"
+)
+
+func TestNewCard(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		number   string
+		bankCard *BankCard
+		wantErr  bool
+	}{
+		{"ok",
+			goodCard,
+			&BankCard{
+				Number:  goodCard,
+				Valid:   false,
+				Checked: false,
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewCard(tt.number)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewCard() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.bankCard) {
+				t.Errorf("NewCard() got = %v, want %v", got, tt.bankCard)
+			}
+		})
+	}
+}
